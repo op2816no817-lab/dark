6708b3fa443b0a10082a67e588ebb1aa82402936
op2816no817-lab <op2816no817@gmail.com>
2026-07-20 07:41:29 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "شف ارثر اسود بس عنصري شوي",     // this is what people actually see
+        state: "تيم دارك كلهم زنوج ما عدا دارك",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
